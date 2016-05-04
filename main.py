f8ffb4a03c6586d8fe5caa4263c5eccb79592d16
lenzzz <401738000@qq.com>
2016-05-04 21:47:22 +0800

    create

---
+++ main.py
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+
+import urllib2,json
+import MySQLdb
+import spider
+
+
+def getLocation(address):
+    url = 'http://api.map.baidu.com/geocoder/v2/'
+    output = 'json'
+    ak = ''
+    uri = url + '?' + 'address=' + address + '&output=' + output + '&ak=' + ak
+    temp = urllib2.urlopen(uri)
+    temp = json.loads(temp.read())
+    if temp['status'] == 0:
+        return temp
+    return False
+
+
+def insertLocationToDatabase():
+    names = spider.returnHospisalName()
+
+    db = MySQLdb.connect("localhost","root","","putianhospital",charset='utf8' )
+
+    for name in names:
+        address_json = getLocation(name)
+        if not address_json:
+            print(name+"GET LOCATION FAILED")
+            continue
+        lng = (address_json['result']['location']['lng'])
+        lat = (address_json['result']['location']['lat'])
+        cursor = db.cursor()
+
+        sql = 'INSERT INTO hospitalLocation VALUES("%s","%s","%s");' %(name,lng,lat)
+        try:
+            cursor.execute(sql)
+        except:
+            print(name+"INSERT FAILED")
+
+    db.close()
+
+
+
+if __name__ == '__main__':
+    insertLocationToDatabase()
---
+++ spider.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+import urllib2
+
+def downTheHTML(url):
+    req_header = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+    'Accept':'text/html;q=0.9,*/*;q=0.8',
+    'Accept-Charset':'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Connection':'close'
+    }
+    req_timeout = 5
+    req = urllib2.Request(url,None,req_header)
+    resp = urllib2.urlopen(req,None,req_timeout)
+    html = resp.read()
+    return html
+
+def returnHospisalName():
+    url = u"http://news.ifeng.com/mainland/special/ptxyy/"
+    html = downTheHTML(url)
+    html = html[html.find("var hospitals = [")+len("var hospitals = [")+1:]
+    html = html[:html.find("];")-1]
+
+    eachLine = html.split('\n')
+    hospitalNames = []
+    for line in eachLine:
+        hospital = line.split("<br/>")
+        hospital[0]=hospital[0][hospital[0].find(',')+2:]
+        hospital[-1]=hospital[-1][:hospital[-1].find(',')-1]
+        for hosp in hospital:
+            hospitalNames.append(hosp)
+    return hospitalNames

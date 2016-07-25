48e1588ff91b2007825cce7997f00f75ca025aa4
lenzzz <401738000@qq.com>
2016-07-25 15:08:15 +0800

    Update getLocation.py

--- getLocation.py
+++ getLocation.py
@@ -8,7 +8,7 @@ import spider
 def getLocation(address):
     url = 'http://api.map.baidu.com/geocoder/v2/'
     output = 'json'
-    ak = 'hpX9BN94G7wEQDOvrdjoDSaKoMN8eiBR'
+    ak = ''
     uri = url + '?' + 'address=' + address + '&output=' + output + '&ak=' + ak
     temp = urllib2.urlopen(uri)
     temp = json.loads(temp.read())
